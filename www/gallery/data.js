837fb7fc71d3ad1355d5d9386b6cf5a1de51ff79
Jonathan Sanchez <jsanchezpando@cirb.irisnet.be>
2012-09-09 14:56:54 +0200

    refact layout, add content, spyscroll

---
+++ www/gallery/data.js
@@ -0,0 +1,83 @@
+var data = {
+	classic: [{
+		title: "Images et passion",
+		artworks: [
+			"gallery/83.jpg",
+			"gallery/84.jpg"
+		]
+	}, {
+		title: "Draperies",
+		artworks: [
+			"gallery/86.jpg"
+		]
+	}, {
+		title: "Exquises esquisses",
+		artworks: [
+			"gallery/86.jpg"
+		]
+	}, {
+		title: "Geishas",
+		artworks: [
+			"gallery/86.jpg"
+		]
+	}, {
+		title: "Femmes... à nos Hymnes",
+		artworks: [
+			"gallery/86.jpg"
+		]
+	}, {
+		title: "Dessins",
+		artworks: [
+			"gallery/86.jpg"
+		]
+	}],
+	erotic: [{
+		title: "Peintures érotiques",
+		artworks: [
+			"gallery/83.jpg",
+			"gallery/84.jpg"
+		]
+	}, {
+		title: "Portraits intimes",
+		artworks: [
+			"gallery/86.jpg"
+		]
+	}, {
+		title: "Dessins érotiques",
+		artworks: [
+			"gallery/86.jpg"
+		]
+	}, {
+		title: "Illustrations du Maître des Plaisirs",
+		artworks: [
+			"gallery/86.jpg"
+		]
+	}],
+	ad: [{
+		title: "Personnages",
+		artworks: [
+			"gallery/83.jpg",
+			"gallery/84.jpg"
+		]
+	}, {
+		title: "Animaux",
+		artworks: [
+			"gallery/86.jpg"
+		]
+	}, {
+		title: "Food",
+		artworks: [
+			"gallery/86.jpg"
+		]
+	}, {
+		title: "Non-Food",
+		artworks: [
+			"gallery/86.jpg"
+		]
+	}, {
+		title: "Autres styles",
+		artworks: [
+			"gallery/86.jpg"
+		]
+	}]
+}
--- www/js/gallery.js
+++ www/js/gallery.js
@@ -6,36 +6,34 @@ $(function () {
         to,
 		count = 0;
 
-	addGalleries(galleryData, menu);
+    for(var cat in data) {
+        $("#" + cat).after(makeGallery(data[cat]));
+    }
 
-	function addGalleries(galleries, conainer) {
-		var menu = $("<div class='gallery'/>").appendTo(conainer);
+	function makeGallery(galleries) {
+		var menu = $("<div class='content btn-group btn-group-vertical'/>");
 		for (i in galleries) {
 			var gallery = galleries[i];
-			if (gallery.galleries) {
-				var elem = $("<div />").appendTo(menu);
-				elem.addClass("btn-group btn-group-vertical");
-				$("<h2>" + gallery.title + "</h2>").addClass("divider").appendTo(elem);
-				addGalleries(gallery.galleries, elem);
-			}
-			if (gallery.photos) {
+
+			if (gallery.artworks) {
 				var elem = $("<a class='btn btn-gallery'>" + gallery.title + "</a>").appendTo(menu);
 				elem.attr("href", "#g" + count);
-				addPhotos(gallery.photos, "g" + count, gallery.title);
+				addArtworks(gallery.artworks, "g" + count, gallery.title);
 				count++;
 			}
 		}
+        return menu;
 	}
 
-	function addPhotos(photos, id, title) {
-		for (i in photos) {
-			var photo = photos[i];
+	function addArtworks(artworks, id, title) {
+		for (i in artworks) {
+			var artwork = artworks[i];
 			var item = $("<div class='item' title='" + title + "' />").appendTo(carInner);
 			if (i == 0) {
 				item.attr("id", id); // first image in given list will be referenced
 			}
 			
-			$("<img src='" + photo + "'>")
+			$("<img src='" + artwork + "'>")
                 .wrap("<div class='v-align'/>").parent()
                 .wrap("<div class='v-align-wrap'/>").parent()
                 .appendTo(item);
@@ -62,4 +60,5 @@ $(function () {
 	$(document.body).delegate('.close', 'click', function () {
 		carousel.slideUp();
 	});
+    $("#main").scrollspy('refresh');
 });
